7d3c43b700166cd5f21adb820565f6b2efec0e8d
Josh Wulf <josh@magikcraft.io>
2019-11-13 10:52:39 +1000

    .

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const CHIRP_URL = process.env.CHIRP_URL || `http://localhost:3000`;
 const SQUAWK_URL = process.env.SQUAWK_URL || "http://localhost:3001";
-const CANARY_HEARTBEAT_SEC = process.env.CANARY_HEARTBEAT_SEC || 300;
+const CANARY_HEARTBEAT_SEC = process.env.CANARY_HEARTBEAT_SEC || 30;
 const DEBUG = process.env.DEBUG || false;
 
 const canary = new ZeebeCanary({
